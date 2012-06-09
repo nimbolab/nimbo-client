bce32efcb3d17f4dd5981f90ec7617314b3e2793
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-06-09 19:53:54 +0200

    Removed Test::Unit tests

--- test/lib/nimbo/version_test.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../../test_helper'
- 
-describe Nimbo do
-  it "must be defined" do
-    Nimbo::VERSION.wont_be_nil
-  end
-end
--- test/test_helper.rb
+++
@@ -1,3 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require File.expand_path('../../lib/nimbo.rb', __FILE__)

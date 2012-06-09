b245ac19d103b34d7061ae8498afcae16402047c
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-06-09 19:52:28 +0200

    Fixed identation

--- lib/nimbo/client.rb
+++ lib/nimbo/client.rb
@@ -1,22 +1,22 @@
 require 'rest_client'
 
 module Nimbo
-	class Client
+  class Client
 
-	  def initialize(service_url, tmp_dir)
-	  	@service_url = service_url
-	  	@tmp_dir = tmp_dir
-	  end
+    def initialize(service_url, tmp_dir)
+      @service_url = service_url
+      @tmp_dir = tmp_dir
+    end
 
-		def archive(suite_dir)
-			suite_archive =  File.join @tmp_dir, 'suite.tar.gz'
-			`tar -cvf #{suite_archive} #{suite_dir}`
-			suite_archive
-		end
+    def archive(suite_dir)
+      suite_archive =  File.join @tmp_dir, 'suite.tar.gz'
+      `tar -cvf #{suite_archive} #{suite_dir}`
+      suite_archive
+    end
 
-		def request(suite_archive)
-			RestClient.post @service_url, :suite_archive => File.new(suite_archive, 'rb')
-		end
-	
-	end
+    def request(suite_archive)
+      RestClient.post @service_url, :suite_archive => File.new(suite_archive, 'rb')
+    end
+
+  end
 end

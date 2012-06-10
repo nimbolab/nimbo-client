63640593a14098fd378e31fad90d0dd39ad4ef06
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-06-10 14:29:40 +0200

    Fixed missing module declaration

--- lib/nimbo/client.rb
+++ lib/nimbo/client.rb
@@ -1,14 +1,15 @@
 require 'rest_client'
 
-class Nimbo::Client
-  attr_accessor :service_url
+module Nimbo
+  class Client
+    attr_accessor :service_url
 
-  def initialize
-    @service_url = 'http://nimbolab.com/test'
-  end
+    def initialize
+      @service_url = 'http://localhost:4000/test'
+    end
 
-  def send(suite)
-    RestClient.post @service_url, :suite_archive => File.new(suite.archive, 'a+')
+    def send(suite)
+      RestClient.post @service_url, :suite_archive => File.new(suite.archive, 'a+')
+    end
   end
 end
-
--- lib/nimbo/suite.rb
+++ lib/nimbo/suite.rb
@@ -1,15 +1,17 @@
-class Nimbo::Client::Suite
-  attr_accessor :tmp_dir
-  attr_reader :suite_dir
+module Nimbo
+  class Client::Suite
+    attr_accessor :tmp_dir
+    attr_reader :suite_dir
 
-  def initialize(suite_dir)
-    @suite_dir = suite_dir
-    @tmp_dir = ENV['TMPDIR']
-  end
+    def initialize(suite_dir)
+      @suite_dir = suite_dir
+      @tmp_dir = ENV['TMPDIR']
+    end
 
-  def archive
-    archive_path = File.join(@tmp_dir, "suite_#{object_id}.tar.gz")
-    success = system("tar -cvf #{archive_path} #{@suite_dir}")
-    archive_path if success
+    def archive
+      archive_path = File.join(@tmp_dir, "suite_#{object_id}.tar.gz")
+      success = system("tar -cvf #{archive_path} #{@suite_dir}")
+      archive_path if success
+    end
   end
 end

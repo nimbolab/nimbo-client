a44d4db054e9e5ae031385ea74ba5df0666013aa
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2012-06-10 18:05:32 +0200

    Changed service url

--- lib/nimbo/client.rb
+++ lib/nimbo/client.rb
@@ -5,7 +5,7 @@ module Nimbo
     attr_accessor :service_url
 
     def initialize
-      @service_url = 'http://localhost:4000/test'
+      @service_url = 'http://nimbolab.com:4000/test'
     end
 
     def send(suite)
